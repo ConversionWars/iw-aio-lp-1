1b77a15602fe387968fc166edf859c93ed6ad7f7
joncraigdesign <38989712+joncraigdesign@users.noreply.github.com>
2024-06-26 13:32:18 -0400

    step

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
         <div class="hero-b">
         <div class="container">
             <div class="row">
-                <div class="col-sm-4 col-sm-offset-5 adj-1">
+                <div class="col-sm-6 col-sm-offset-4 adj-1">
                     <h1>Tired of the same old<br /> Wellness Routine?</h1>
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-8 adj-2">
+                <div class="col-sm-12 adj-2">
                     <h2 class="white">Break the <span class="red">Mold</span></h2>
                 </div>
             </div>
@@ -30,6 +30,59 @@
                     <img src="img/t1-aso.webp" class="img-responsive" />
                 </div>
             </div>
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <p class="white">
+                    Our gummies pack a punch with supernatural ingredients, zero artificial nonsense, and a taste so good it'll make your taste buds go viral. They're gluten-free, vegan-friendly, and ready to rock your world.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 adj-5">
+                    <h3 class="pink">Designed for the <span class="txt-outline">elite</span>, the <span class="txt-outline">unstoppable</span>, and the <span class="txt-outline">fearless.</span></h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 adj-6">
+                    <img src="img/t2-cir-left.webp" class="img-responsive" />
+                </div>
+                <div class="col-sm-4 adj-7">
+                    <img src="img/t2-cir-right.webp" class="img-responsive" />
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <div class="bar bl-gr"></div>
+
+    <div class="tier-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="pink h-shad">Discover Your <br />Superpowers!</h4>
+                </div>
+            </div>
+            <div class="row adj-1">
+                <div class="col-sm-5 hbox">
+                    <h3><span class="green">Unleash</span> Your Mind's Potential</h3>
+                    <div class="btl-ttl">Focus</div>
+                </div>
+                <div class="col-sm-3 col-sm-offset-2">
+                    <h5>Iwanna <span class="green">Focus</span></h5>
+                    <p>Ignite your cognitive fireworks with an explosive mix of brain-boosting berries.</p>
+                    <div class="cta">
+                        <a href="#" class="cta-btn green_bg">Get Focus</a>
+                    </div>
+                </div>
+            </div>
+            <div class="btl-con">
+                <img src="img/btl-f.webp" class="img-responsive" />
+                <div class="btl-snip">
+                    <span class="s1">Your Daily Dose of</span><br />
+                    <span class="s2"> Concentration</span><br />
+                    <span class="s3"> &amp; Clarity</span>
+                </div>
             </div>
         </div>
     </div>
--- template/header.php
+++ template/header.php
@@ -9,7 +9,7 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Braah+One&family=Bruno+Ace+SC&family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Braah+One&family=Bruno+Ace+SC&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 
     <link href="css/reset.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
