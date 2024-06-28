1f7faecc4f548744cdb9bbd62b3d6e54bd074fed
joncraigdesign <38989712+joncraigdesign@users.noreply.github.com>
2024-06-27 20:56:45 -0400

    Closer

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <h4 class="pink h-shad">Discover Your <br />Superpowers!</h4>
+                    <h4 class="pink stroke">Discover Your <br />Superpowers!</h4>
                 </div>
             </div>
             <div class="row adj-1">
@@ -94,32 +94,245 @@
     <div class="tier-4">
         <div class="container">
             <div class="row adj-1">
-                <div class="col-sm-5 hbox pos">
-                    <h3><span class="green">Unleash</span> Your Mind's Potential</h3>
-                    <div class="btl-ttl">Focus</div>
+                <div class="col-sm-5 col-sm-push-7 hbox pos">
+                    <h3>Start Your Day With <span class="red">Energy</span></h3>
+                    <div class="btl-ttl">WakeUp</div>
                     <div class="line"></div>
                 </div>
-                <div class="col-sm-3 col-sm-offset-2 hbox adj-2">
-                    <h5>Iwanna <span class="green">Focus</span></h5>
-                    <p>Ignite your cognitive fireworks with an explosive mix of brain-boosting berries.</p>
+                <div class="col-sm-3 col-sm-pull-3 hbox adj-2">
+                    <h5>Iwanna <span class="red">WakeUp</span></h5>
+                    <p>Electrify your morning routine with a surge of citrus, more refreshing than a viral tweet.</p>
                     <div class="cta">
-                        <a href="#" class="cta-btn green_bg">Get Focus</a>
+                        <a href="#" class="cta-btn red_bg">Get Wakeup</a>
                     </div>
                     <div class="line"></div>
                 </div>
             </div>
             <div class="btl-con">
-                <img src="img/btl-f.webp" class="img-responsive" />
-                <div class="btl-snip text-right">
-                    <span class="s1">Your Daily Dose of</span><br />
-                    <span class="s2"> Concentration</span><br />
-                    <span class="s3"> &amp; Clarity</span>
+                <img src="img/btl-g.webp" class="img-responsive" />
+                <div class="btl-snip white">
+                    <span class="s1">Your Morning</span><br />
+                    <span class="s2"> Boost For a</span><br />
+                    <span class="s3"> Fresh Start</span>
                 </div>
             </div>
         </div>
     </div>
 
+    <div class="bar bl-pi"></div>
+
+    <div class="tier-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ul>
+                        <li class="s1">Enhanced <span class="green">Concentration</span></li>
+                        <li class="s2">Morning <span class="red">Energy Boost</span></li>
+                        <li class="s3">Tasty <span class="pink">Stress Reducer</span></li>
+                        <li class="s4">Quick <span class="purple">Sleep Induction</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bar pi-bl"></div>
+
+    <div class="tier-6">
+        <div class="container">
+            <div class="row adj-1">
+                <div class="col-sm-5 hbox pos">
+                    <h3>Unwind, Unplug, And <span class="pink">De-Stress</span></h3>
+                    <div class="btl-ttl">Relax</div>
+                    <div class="line"></div>
+                </div>
+                <div class="col-sm-3 col-sm-offset-2 hbox adj-2">
+                    <h5>Iwanna <span class="pink">Relax</span></h5>
+                    <p>Dissolve stress with a calming lavender cloud that soothes your senses like a smooth playlist.</p>
+                    <div class="cta">
+                        <a href="#" class="cta-btn pink_bg">Get Relax</a>
+                    </div>
+                    <div class="line"></div>
+                </div>
+            </div>
+            <div class="btl-con">
+                <img src="img/btl-r.webp" class="img-responsive" />
+                <div class="btl-snip text-right">
+                    <span class="s1">Your Key to</span><br />
+                    <span class="s2"> Calm &amp;</span><br />
+                    <span class="s3"> Serenity</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bar bl-pu"></div>
+
     <div class="tier-7">
+        <div class="container">
+            <div class="row adj-1">
+                <div class="col-sm-5 col-sm-push-7 hbox pos">
+                    <h3>Drift Into <span class="purple">Dreamland</span></h3>
+                    <div class="btl-ttl">Sleep</div>
+                    <div class="line"></div>
+                </div>
+                <div class="col-sm-3 col-sm-pull-3 hbox adj-2">
+                    <h5>Iwanna <span class="purple">Sleep</span></h5>
+                    <p>Sink into a blissful slumber with chamomile and melatonin, sweeter than your wildest dreams.</p>
+                    <div class="cta">
+                        <a href="#" class="cta-btn purple_bg">Get Sleep</a>
+                    </div>
+                    <div class="line"></div>
+                </div>
+            </div>
+            <div class="btl-con">
+                <img src="img/btl-s.webp" class="img-responsive" />
+                <div class="btl-snip white">
+                    <span class="s1">Your Nightly</span><br />
+                    <span class="s2"> Companion For</span><br />
+                    <span class="s3"> Restful Sleep</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bar pu-bl"></div>
+
+    <div class="tier-8">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-3 ico-con">
+                    <div class="ico-img"><img src="img/t8-ico-1.webp" class="img-responsive" /></div>
+                    <h4>Premium <br />Ingredients</h4>
+                    <p>All our products are made in GMP certified labs from premium quality extracts and ingredients to ensure you have access to only the best.</p>
+                </div>
+                <div class="col-xs-6 col-sm-3 ico-con">
+                    <div class="ico-img"><img src="img/t8-ico-2.webp" class="img-responsive" /></div>
+                    <h4>Fast Acting <br />Formulations</h4>
+                    <p>Premium ingredients and vitamin infusion designed to deliver rapid absorption and premium results.</p>
+                </div>
+                <div class="clearfix visible-xs"></div>
+                <div class="col-xs-6 col-sm-3 ico-con">
+                    <div class="ico-img"><img src="img/t8-ico-3.webp" class="img-responsive" /></div>
+                    <h4>90 Day <br />Guarantee</h4>
+                    <p>If you aren’t in love with our gummies simply return your bottle after 90 days for a full refund.</p>
+                </div>
+                <div class="col-xs-6 col-sm-3 ico-con">
+                    <div class="ico-img"><img src="img/t8-ico-4.webp" class="img-responsive" /></div>
+                    <h4>24-7 <br />Customer Care</h4>
+                    <p>Our team are here to help, 24 hours a day, 7 days a week. Contact our premium care team for instant answers to any questions you may have.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bar bl-pu"></div>
+
+    <div class="tier-9">
+        <div class="tier-9b">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4 class="text-center black white-wk stroke">What Makes iWanna Gummies <span class="pink">so Special?</span></h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-2 adj-1">
+                        <p>Packed with nature's wonders like ginkgo biloba for that mental edge, ginseng for stamina, melissa extract for calm, plus the essential kick of B vitamins. It's nature's best, bottled up in a gummy.</p>
+
+                        <p>Each gummy is a bite-sized powerhouse, crafted with all-natural ingredients and zero artificial nonsense. Gluten-free, vegan-friendly, and absolutely delicious—our gummies are designed for everyone looking for an easy and enjoyable way to boost their daily health.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-5">
+                        <img src="img/t9-ing-1.webp" class="img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-sm-pull-1 adj-2">
+                        <div class="hbox">
+                            <div class="line"></div>
+                            <h6>Ginkgo Biloba</h6>
+                            <p>Revered for its memory-enhancing properties, this natural extract boosts cerebral blood flow, sharpening your focus and illuminating your path to peak cognitive performance.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-5 col-sm-push-7">
+                        <img src="img/t9-ing-2.webp" class="img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-sm-pull-3 adj-3">
+                        <div class="hbox">
+                            <div class="line"></div>
+                            <h6>Ginseng</h6>
+                            <p class="text-right">This time-honored ingredient is known for its ability to fight fatigue and enhance brain function, giving you the sustained energy and mental clarity needed to conquer your most demanding days.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-5">
+                        <img src="img/t9-ing-3.webp" class="img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-sm-pull-1 adj-4">
+                        <div class="hbox">
+                            <div class="line"></div>
+                            <h6>Gaba</h6>
+                            <p>Gamma-aminobutyric acid (GABA) is a neurotransmitter that promotes relaxation, reduces anxiety, and improves sleep quality by calming the nervous system.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-5 col-sm-push-7">
+                        <img src="img/t9-ing-4.webp" class="img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-sm-pull-3 adj-5">
+                        <div class="hbox">
+                            <div class="line"></div>
+                            <h6>Ashwagandha</h6>
+                            <p class="text-right">An adaptogenic herb known for reducing stress and anxiety, enhancing mood, supports cognitive function, boosts energy levels, and promotes better sleep.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bar pi-bl"></div>
+
+    <div class="tier-10">
+        <div class="container">
+            <div class="row adj-1">
+                <div class="col-sm-10 col-sm-offset-1 hbox">
+                    <h2 class="text-center">
+                        <span class="green">R</span><span class="red">e</span><span class="pink">a</span><span class="purple">l</span> People. <br class="visible-xs" /><span class="green">R</span><span class="red">e</span><span class="pink">a</span><span class="purple">l</span> Reviews.
+                    </h2>
+                    <div class="line"></div>
+                </div>
+            </div>
+            <div class="row adj-2">
+                <div class="col-sm-4">
+                    <img src="img/t10-tes-1.webp" class="img-responsive" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="img/t10-tes-2.webp" class="img-responsive" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="img/t10-tes-3.webp" class="img-responsive" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bar bl-pi"></div>
+
+    <div class="tier-12">
         <div class="container">
             <div class="row">
                 <div class="col-sm-4 col-lg-6 col-sm-offset-4 col-lg-offset-3 hbox hidden-xs">
